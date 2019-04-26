9839478eedcf4bf95e68df91a779ed2ca68788ed
dkarris <dkarlov2005@gmail.com>
2019-04-26 14:52:55 -0400

    Week 6 started

--- Week 5/ex4/ex4.m
+++ Week 5/ex4/ex4.m
@@ -40,8 +40,8 @@ sel = sel(1:100);
 
 displayData(X(sel, :));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+fprintf('Program kbhit()d. Press enter to continue.\n');
+kbhit();
 
 
 %% ================ Part 2: Loading Parameters ================
@@ -79,8 +79,8 @@ J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
 fprintf(['Cost at parameters (loaded from ex4weights): %f '...
          '\n(this value should be about 0.287629)\n'], J);
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+fprintf('\nProgram kbhit()d. Press enter to continue.\n');
+kbhit();
 
 %% =============== Part 4: Implement Regularization ===============
 %  Once your cost function implementation is correct, you should now
@@ -98,8 +98,8 @@ J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
 fprintf(['Cost at parameters (loaded from ex4weights): %f '...
          '\n(this value should be about 0.383770)\n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+fprintf('Program kbhit()d. Press enter to continue.\n');
+kbhit();
 
 
 %% ================ Part 5: Sigmoid Gradient  ================
@@ -115,8 +115,8 @@ fprintf('Sigmoid gradient evaluated at [-1 -0.5 0 0.5 1]:\n  ');
 fprintf('%f ', g);
 fprintf('\n\n');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+fprintf('Program kbhit()d. Press enter to continue.\n');
+kbhit();
 
 
 %% ================ Part 6: Initializing Pameters ================
@@ -145,8 +145,8 @@ fprintf('\nChecking Backpropagation... \n');
 %  Check gradients by running checkNNGradients
 checkNNGradients;
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+fprintf('\nProgram kbhit()d. Press enter to continue.\n');
+kbhit();
 
 
 %% =============== Part 8: Implement Regularization ===============
@@ -167,8 +167,8 @@ debug_J  = nnCostFunction(nn_params, input_layer_size, ...
 fprintf(['\n\nCost at (fixed) debugging parameters (w/ lambda = %f): %f ' ...
          '\n(for lambda = 3, this value should be about 0.576051)\n\n'], lambda, debug_J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+fprintf('Program kbhit()d. Press enter to continue.\n');
+kbhit();
 
 
 %% =================== Part 8: Training NN ===================
@@ -204,8 +204,8 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+fprintf('Program kbhit()d. Press enter to continue.\n');
+kbhit();
 
 
 %% ================= Part 9: Visualize Weights =================
@@ -217,8 +217,8 @@ fprintf('\nVisualizing Neural Network... \n')
 
 displayData(Theta1(:, 2:end));
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+fprintf('\nProgram kbhit()d. Press enter to continue.\n');
+kbhit();
 
 %% ================= Part 10: Implement Predict =================
 %  After training the neural network, we would like to use it to predict
